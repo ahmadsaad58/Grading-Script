0062d297122e5a01cf05d823ae687097d1d631d1
Saad Ahmad <ahmadsaad58@gmail.com>
2020-01-16 01:43:26 -0600

    created unittest framework

---
+++ Unit_Test.py
@@ -0,0 +1,16 @@
+import unittest
+from Sample import greet, bye
+
+# Unit Test Class
+class TestSample(unittest.TestCase):
+    
+    # Test the greet function
+    def test_greet(self):
+        self.assertEqual(greet("Saad"), "Hello, Saad!", "Should be \'Hello, Saad!\'")
+
+    # Test the bye function
+    def test_bye(self):
+        self.assertEqual(bye(), "Good Bye!", "Should be \'Good Bye!\'")
+
+if __name__ == '__main__':
+    unittest.main()
