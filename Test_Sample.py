dd63e9e355e2d017fe196cb9fcc7099f285cf4b3
Saad Ahmad <ahmadsaad58@gmail.com>
2020-01-16 18:43:48 -0600

    Added timeout feature

--- Test_Sample.py
+++ Test_Sample.py
@@ -1,14 +1,21 @@
 import unittest
-from Sample import greet, bye
+
+# decorator for time
+from Timeout import timeout
+
+# individual student copied file, put all the functions you are testing 
+from Test import greet, bye
 
 # Unit Test Class
 class Test_Sample(unittest.TestCase):
     
     # Test the greet function
+    @timeout()
     def test_greet(self):
         self.assertEqual(greet("Saad"), "Hello, Saad!", "Should be \'Hello, Saad!\'")
 
     # Test the bye function
+    @timeout()
     def test_bye(self):
         self.assertEqual(bye(), "Good Bye!", "Should be \'Good Bye!\'")
 
